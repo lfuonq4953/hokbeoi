45ccaad820d73b27926e8950549f9572b77f7eaa
lfuonq4953 <99533043+lfuonq4953@users.noreply.github.com>
2022-02-12 16:51:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
     titleWeb: "Chao Cau",
     introTitle: 'Hong bic la ai',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `dkm cau ngu nhu tro`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    title: 'cau nhin ngu quaa',
+    desc: 'cut dy ',
+    btnYes: 'hok be oi ',
+    btnNo: 'o k e',
+    question: 'sao cau hok cut di',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    reply: 'tai to thich cau',
+    mess: 'to biet ma',
+    messDesc: '',
+    btnAccept: '\bie',
     messLink: 'https://www.facebook.com/messages/t/100034537841831' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
